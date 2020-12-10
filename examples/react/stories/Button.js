cf35f8d6cf27312b3d5567bdbf5eba3579bba2dc
Fredrik Johansson <jeanfredrik@gmail.com>
2020-12-10 12:54:23 +0100

    style: format files

--- examples/react/stories/Button.js
+++ examples/react/stories/Button.js
@@ -6,11 +6,15 @@ import './button.css';
  * Primary UI component for user interaction
  */
 export const Button = ({ primary, backgroundColor, size, label, ...props }) => {
-  const mode = primary ? 'storybook-button--primary' : 'storybook-button--secondary';
+  const mode = primary
+    ? 'storybook-button--primary'
+    : 'storybook-button--secondary';
   return (
     <button
       type="button"
-      className={['storybook-button', `storybook-button--${size}`, mode].join(' ')}
+      className={['storybook-button', `storybook-button--${size}`, mode].join(
+        ' ',
+      )}
       style={backgroundColor && { backgroundColor }}
       {...props}
     >
