955a1184e747f25f534faa480101802ca5ec2eac
Dabiel González Ramos <dabiel.ramos@endava.com>
2022-09-23 14:20:42 +0300

    feat: add removeComments parameter
    It will allow removing all lit comments or, if a valid RegExp has been set, it will remove all comments matching the expression.

--- addon/src/decorators/withHTML.js
+++ addon/src/decorators/withHTML.js
@@ -1,6 +1,15 @@
 import { addons, makeDecorator } from '@storybook/addons';
 import { EVENT_CODE_RECEIVED } from '../shared';
 
+const isValidRegExp = (expression) => {
+  try {
+    new RegExp(expression);
+  } catch (e) {
+    throw new Error(e.message);
+  }
+  return true;
+};
+
 export const withHTML = makeDecorator({
   name: 'withHTML',
   parameterName: 'html',
@@ -13,6 +22,16 @@ export const withHTML = makeDecorator({
       let html = root ? root.innerHTML : `${rootSelector} not found.`;
       if (parameters.removeEmptyComments) {
         html = html.replace(/<!--\s*-->/g, '');
+      }
+      if (parameters.removeComments) {
+        const { removeComments } = parameters;
+        if (typeof removeComments === 'boolean') {
+          // Will remove all HTML comments
+          html = html.replace(/<!--(.*?)-->/g, '');
+        } else if (isValidRegExp(removeComments)) {
+          // Will remove only HTML comments matching the expression provided
+          html = html.replace(removeComments, '');
+        }
       }
       channel.emit(EVENT_CODE_RECEIVED, { html, options: parameters });
     }, 0);
