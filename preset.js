5bacd84ada8b89931bc2ff4b0cf6f9f2e0e03050
legobt <6wbvkn0j@anonaddy.me>
2023-05-19 11:38:56 +0000

    storybook 7 support

--- preset.js
+++ preset.js
@@ -6,7 +6,12 @@ function managerEntries(entry = []) {
   return [...entry, require.resolve("./dist/esm/preset/manager")];
 }
 
+function previewAnnotations(entry = []) {
+  return [...entry, require.resolve("./dist/esm/preset/manager")];
+}
+
 module.exports = {
   managerEntries,
   config,
+  previewAnnotations,
 };
