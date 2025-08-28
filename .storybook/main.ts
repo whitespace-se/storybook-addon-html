c6cf2274afdcb7ce997154dd5b9e889c53d1c883
William Perkola <124706876+williamp-osttra@users.noreply.github.com>
2025-08-28 21:53:06 +0200

    Upgrade to Storybook 9 (#140)
    Co-authored-by: William Perkola <william.perkola@trioptima.com>

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,18 +1,11 @@
 import type { StorybookConfig } from "@storybook/react-vite";
 const config: StorybookConfig = {
   stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
-  addons: [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-interactions",
-    "./local-preset.js",
-  ],
+  addons: ["@storybook/addon-links", "@storybook/addon-docs", "./local-preset.js"],
+
   framework: {
     name: "@storybook/react-vite",
     options: {},
-  },
-  docs: {
-    autodocs: "tag",
-  },
+  }
 };
 export default config;
